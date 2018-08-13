5255813e5ee8ec76e5e972c88ffe975747165c0b
Marlom Girardi <marlom.girardi@ideal-sumario.pt>
2018-08-13 10:28:48 +0100

    rest and pagination changes

--- graphql/directives/rest.js
+++ graphql/directives/rest.js
@@ -3,23 +3,18 @@ import fetch from 'node-fetch'
 
 const convertObjectToArray = (object) => Object.getOwnPropertyNames(object).map(prop => object[prop])
 
-
 export default class RestDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
     const { url, type } = this.args;
-    field.resolve = (source, { login }, context, info) => {
+    field.resolve = (source, props, context, info) => {
       let customUrl = url
-      if (login) {
-          customUrl += `/${login}`
-      }
-
       const re = /{[a-zA-Z0-9-_.]+}/g
 
       let result
       while (result = re.exec(url)) {
         const found = result[0]
         const param = found.replace(/{|}/g, '')
-        const value = source[param]
+        const value = props[param] ? props[param] : source[param]
 
         if (typeof value === "undefined") {
           throw new Error('Invalid param ' + param)
